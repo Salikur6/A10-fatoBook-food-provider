36131c0e137a243e44e7b3c37fc61e68ad1b0543
salikur6 <salikur6@gmail.com>
2022-04-19 02:50:30 +0600

    additional section Review is designed

---
+++ src/Components/Home/Reviews/Reviews.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const Reviews = () => {
+    return (
+        <div className='container'>
+            <div className='pt-5 mb-3'>
+                <h2 className='text-center fw-bold'>Reviews</h2>
+                <div className='d-flex justify-content-center'>
+                    <div className='bg-danger' style={{ height: '2px', width: '20%' }}></div>
+                </div>
+                <div className='d-flex justify-content-center mt-1'>
+                    <div className='bg-danger' style={{ height: '2px', width: '15%' }}></div>
+                </div>
+                <div className='d-flex justify-content-center mt-1'>
+                    <div className='bg-danger' style={{ height: '2px', width: '10%' }}></div>
+                </div>
+            </div>
+
+
+            <div className="row g-5 my-4">
+                <div className="col-lg-4 col-md-6">
+                    <Card className='p-3'>
+                        <div className='d-flex align-items-center'>
+                            <img style={{ width: '75px', borderRadius: '50px' }} src='https://images.generated.photos/PcwRAjIzAvt1njc47jeYDmiR1Ip1yyC-MHqwl-xWX4k/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MzMyNzk4LmpwZw.jpg' alt="" />
+                            <h5 className='m-3 fw-bold'>Saifur Rahman</h5>
+                        </div>
+                        <p className='my-3'>Coffee was fresh, the corned beef hash is amazing and homemade, even the toast was good! Oh, and the potato pancakes! Everything we ordered was fresh and delicious. Highly recommend! Definitely coming back.</p>
+                    </Card>
+                </div>
+                <div className="col-lg-4 col-md-6">
+                    <Card className='p-3'>
+                        <div className='d-flex align-items-center'>
+                            <img style={{ width: '75px', borderRadius: '50px' }} src='https://images.generated.photos/942PEQT_pQWxGwvnvmG5Iq28LvmCmYxKuJf9IWTIJa4/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NzgzMzQ4XzA1MTgx/OTJfMDEzODg2MC5q/cGc.jpg' alt="" />
+                            <h5 className='m-3 fw-bold'>Salikur Islam</h5>
+                        </div>
+                        <p className='my-3'>The food was excellent and so was the service.  I had the mushroom risotto with scallops which was awesome. My wife had a burger over greens (gluten-free) which was also very good.</p>
+                    </Card>
+                </div>
+                <div className="col-lg-4 col-md-6">
+                    <Card className='p-3'>
+                        <div className='d-flex align-items-center'>
+                            <img style={{ width: '75px', borderRadius: '50px' }} src='https://images.generated.photos/aEUkAFomUvukyYrHltNi5yCwcKB4nM1h-cpyFAkK-mg/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NzgzMzQ4XzA1MTgx/OTJfMDA5NTkyMy5q/cGc.jpg' alt="" />
+                            <h5 className='m-3 fw-bold'>Sofiul Rahman</h5>
+                        </div>
+                        <p className='my-3'>Great selection! My go to favorite is the Cape Cod Ruben. We also love their pizza. I just had the mushroom risotto with scallops and loved it!</p>
+                    </Card>
+                </div>
+
+                <div className="col-lg-4 col-md-6">
+                    <Card className='p-3'>
+                        <div className='d-flex align-items-center'>
+                            <img style={{ width: '75px', borderRadius: '50px' }} src='https://images.generated.photos/OA3Sdj8vrIGDQOkoNHFPBfgWQAoHqQA1WprKR3_spLE/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MjkxMjcxLmpwZw.jpg' alt="" />
+                            <h5 className='m-3 fw-bold'>Abdul Alim</h5>
+                        </div>
+                        <p className='my-3'>Great pizza and brunch as well. We had a big party for my birthday brunch and our waitress Maddie was so nice and awesome even with our big loud table of 11 people. I had the garden Benedict which was great, as well as the pancakes.</p>
+                    </Card>
+                </div>
+
+                <div className="col-lg-4 col-md-6">
+                    <Card className='p-3'>
+                        <div className='d-flex align-items-center'>
+                            <img style={{ width: '75px', borderRadius: '50px' }} src='https://images.generated.photos/XPijPoysbJvnEiKlk9olAeuw-HGn2GDkON_yQxrFgOU/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MzQ1ODg2XzA3OTY1/NTZfMDgwMjQzNy5q/cGc.jpg' alt="" />
+                            <h5 className='m-3 fw-bold'>Saifur Mahi</h5>
+                        </div>
+                        <p className='my-3'>Great pizza, mozzarella sticks, calamari, chicken tenders, I could go on. Everything we’ve tried here has been amazing. The staff is always so friendly and accommodating.</p>
+                    </Card>
+                </div>
+
+                <div className="col-lg-4 col-md-6">
+                    <Card className='p-3'>
+                        <div className='d-flex align-items-center'>
+                            <img style={{ width: '75px', borderRadius: '50px' }} src='https://images.generated.photos/xt9vxHdNu69umrT3DsmrR_c6InoNdos9KvhK6_53G4k/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NjYzODQ4LmpwZw.jpg' alt="" />
+                            <h5 className='m-3 fw-bold'>Jalal Ahmed</h5>
+                        </div>
+                        <p className='my-3'>Been coming here for 20+ years. Never disappointed. Good service combined with good food.</p>
+                    </Card>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Reviews;
--- src/Components/Home/Service/Service.js
+++ src/Components/Home/Service/Service.js
@@ -5,7 +5,7 @@ import './Service.css'
 const Service = ({ service }) => {
     const { id, img, name, price, description } = service;
     return (
-        <div className='col-lg-4'>
+        <div className='col-lg-4 col-md-6'>
             <Card className='service-card border-0' style={{ borderRadius: '40px ' }}>
 
                 <Card.Img variant="top" className='w-100' style={{ height: '295px', borderRadius: '40px 40px 0 0' }} src={img} />
